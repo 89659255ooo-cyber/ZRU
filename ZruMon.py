70f60df0d05af806d00980248d48531fdfe6c471
YaschinAA <58318728+YaschinAA@users.noreply.github.com>
2025-09-01 13:13:31 +0500

    Update ZruMon.py 501 12
    501 12

--- ZruMon.py
+++ ZruMon.py
@@ -12,7 +12,7 @@ import sys
 import modbus_tk
 import modbus_tk.defines as cst
 from modbus_tk import modbus_tcp
-revForPO = "11";
+revForPO = "12";
 StertCmdForModBus = "set_values 1 3 1 4 5 6 7 8 7 "+revForPO;
 CmdDateForModBus = "1 3 1 4 5 6 7 8 7 "+revForPO;
 cmdForModBus = StertCmdForModBus
@@ -741,6 +741,7 @@ tServer.start()
 dpg.start_dearpygui()
 
 dpg.destroy_context()
+
 
 
 

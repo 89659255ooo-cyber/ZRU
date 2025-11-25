8875b8ed780d421e02ef2b989c6db4ee2c8037b3
YaschinAA <58318728+YaschinAA@users.noreply.github.com>
2025-11-25 11:28:16 +0500

    Update ZruMon.py
    add cvtotrybt 0

--- ZruMon.py
+++ ZruMon.py
@@ -14,11 +14,11 @@ import modbus_tk.defines as cst
 from modbus_tk import modbus_tcp
 import struct
 
-revForPO = "23";
+revForPO = "24";
 StertCmdForModBus = "set_values 1 3 1 4 5 6 7 8 7 "+revForPO;
 CmdDateForModBus = "1 3 1 4 5 6 7 8 7 "+revForPO;
 cmdForModBus = StertCmdForModBus
-wrRegAddr = 500
+wrRegAddr = 0
 
 import requests
 import os
